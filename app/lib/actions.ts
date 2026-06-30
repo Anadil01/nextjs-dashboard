3a949ec3eeeb36b7a02fe9098c06c7718b236bd5
Anadil <mdanadil32@gmail.com>
2026-06-30 21:12:41 +0530

    Error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,15 +25,21 @@ export async function createInvoice(formData: FormData) {
   
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
+try{
 
   await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
 
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
-
+}catch (error) {
+  // We'll also log the error to the console for now
+  console.error(error);
+  return {
+    message: 'Database Error: Failed to Create Invoice.',
+  };
+}
+revalidatePath('/dashboard/invoices');
+redirect('/dashboard/invoices');
 }
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
@@ -48,11 +54,17 @@ export async function updateInvoice(id: string, formData: FormData) {
    
     const amountInCents = amount * 100;
    
-    await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
+    try {
+      await sql`
+          UPDATE invoices
+          SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+          WHERE id = ${id}
+        `;
+    } catch (error) {
+      // We'll also log the error to the console for now
+      console.error(error);
+      return { message: 'Database Error: Failed to Update Invoice.' };
+    }
    
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -60,6 +72,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 
   export async function deleteInvoice(id: string) {
+    throw new Error('Failed to Delete Invoice');
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
   }
